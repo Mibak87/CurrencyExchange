ed56fac55b787bd3f29a5a6da54bd16ba608f235
Mikhail Bakulin <mibak@yandex.ru>
2023-12-25 11:48:07 +0400

    Realized POST-request of the CurrenciesServlet and ExchangeRatesServlet servlets.

--- src/main/java/main/servlets/CurrenciesServlet.java
+++ src/main/java/main/servlets/CurrenciesServlet.java
@@ -17,7 +17,6 @@ public class CurrenciesServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         ObjectMapper objectMapper = new ObjectMapper();
         PrintWriter print = response.getWriter();
-
         List<Currencies> currencies = new CurrenciesDao().getAll();
         objectMapper.writeValue(print,currencies);
         System.out.println(print);
@@ -25,6 +24,19 @@ public class CurrenciesServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("name");
+        String code = request.getParameter("code");
+        String sign = request.getParameter("sign");
+        Currencies currencies = new Currencies();
+        currencies.setFullName(name);
+        currencies.setCode(code);
+        currencies.setSign(sign);
+        new CurrenciesDao().save(currencies);
 
+        ObjectMapper objectMapper = new ObjectMapper();
+        PrintWriter print = response.getWriter();
+        Currencies responseCurrencies = new CurrenciesDao().getByCode(code);
+        objectMapper.writeValue(print,currencies);
+        System.out.println(print);
     }
 }
--- src/main/java/main/servlets/ExchangeRatesServlet.java
+++ src/main/java/main/servlets/ExchangeRatesServlet.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
+import main.dao.CurrenciesDao;
 import main.dao.ExchangeRatesDao;
 import main.entity.ExchangeRates;
 
@@ -17,7 +18,6 @@ public class ExchangeRatesServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         ObjectMapper objectMapper = new ObjectMapper();
         PrintWriter print = response.getWriter();
-
         List<ExchangeRates> exchangeRates = new ExchangeRatesDao().getAll();
         objectMapper.writeValue(print,exchangeRates);
         System.out.println(print);
@@ -25,6 +25,20 @@ public class ExchangeRatesServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String baseCurrencyCode = request.getParameter("baseCurrencyCode");
+        String targetCurrencyCode = request.getParameter("targetCurrencyCode");
+        String rateString = request.getParameter("rate");
+        double rate = Double.parseDouble(rateString);
+        ExchangeRates exchangeRates = new ExchangeRates();
+        exchangeRates.setBaseCurrencyId(new CurrenciesDao().getByCode(baseCurrencyCode));
+        exchangeRates.setTargetCurrencyId(new CurrenciesDao().getByCode(targetCurrencyCode));
+        exchangeRates.setRate(rate);
+        new ExchangeRatesDao().save(exchangeRates);
 
+        ObjectMapper objectMapper = new ObjectMapper();
+        PrintWriter print = response.getWriter();
+        ExchangeRates responceExchangeRates = new ExchangeRatesDao().getByCode(baseCurrencyCode + targetCurrencyCode);
+        objectMapper.writeValue(print,responceExchangeRates);
+        System.out.println(print);
     }
 }
