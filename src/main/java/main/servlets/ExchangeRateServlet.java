023f44efe84e4c68e9d4507e58c3689ce09be811
Mikhail Bakulin <mibak@yandex.ru>
2023-12-25 15:18:28 +0400

    Realized PATCH-request of the ExchangeRateServlet.

--- src/main/java/main/servlets/ExchangeRateServlet.java
+++ src/main/java/main/servlets/ExchangeRateServlet.java
@@ -24,6 +24,33 @@ public class ExchangeRateServlet extends HttpServlet {
     }
 
     @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String method = request.getMethod();
+        if (method.equalsIgnoreCase("PATCH")) {
+            this.doPatch(request, response);
+        } else {
+            super.service(request, response);
+        }
+    }
+
+    protected void doPatch(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String exchangeCode = request.getPathInfo().substring(1);
+        if (!exchangeCode.isEmpty()) {
+            ExchangeRates exchangeRates = new ExchangeRatesDao().getByCode(exchangeCode);
+            String rateString = request.getParameter("rate");
+            double rate = Double.parseDouble(rateString);
+            exchangeRates.setRate(rate);
+            new ExchangeRatesDao().update(exchangeRates);
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            PrintWriter print = response.getWriter();
+            ExchangeRates responseExchange = new ExchangeRatesDao().getByCode(exchangeCode);
+            objectMapper.writeValue(print,responseExchange);
+            System.out.println(print);
+        }
+    }
+
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
