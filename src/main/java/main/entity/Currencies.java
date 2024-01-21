5cf85f549fa9aa0acbd87edc678787c40886f519
Mikhail Bakulin <mibak@yandex.ru>
2024-01-21 21:38:28 +0300

    Fix a bug.

--- src/main/java/main/entity/Currencies.java
+++ src/main/java/main/entity/Currencies.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
-@JsonPropertyOrder({"id", "code", "fullName", "sign"})
+@JsonPropertyOrder({"id", "fullName", "code", "sign"})
 public class Currencies {
     private int id;
     private String code;
